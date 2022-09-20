cc10904774374917eddfc229b94082af2c70c6f7
Bumtarata <svickovak@gmail.com>
2022-09-20 12:50:20 +0200

    Basic synchronization now works properly.
    On previous commit syncing of subdirs of subdirs (etc.) wouldn't
    work. Now it works properly. Script now also checks last modified
    time of all files contained in both src and dest paths and if they
    don't match, the file is copied from src to dest path.

--- task.py
+++ task.py
@@ -1,26 +1,70 @@
-import shutil, sys
+import shutil, sys, os
 from pathlib import Path
 
-# get the paths of source and destination
-source_path = Path(sys.argv[1])
-dest_path = Path(sys.argv[2])
-
-# Check if destination directory already exists (if this script already
-# run so part of the source directory is copied.
-if not dest_path.is_dir():
-    # Destination directory doesn't exist so copy the whole source directory
-    # tree
-    shutil.copytree(source_path, dest_path)
+class SynchDirs():
+    """Class used for synchronizing content of destination directory according
+    to source directory"""
+    def __init__(self, source_path, destination_path):
+        """Initialize all necessary data."""
+        # get the paths of source and destination
+        self.source_path = source_path
+        self.dest_path = destination_path
     
-else:
-    # Destination dir exists so for every file or dir in source dir check if
-    # it also is in destination dir and if it isn't there, copy it.
-    for child in source_path.iterdir():
-        dest_path_of_child = dest_path / child.name
-        if dest_path_of_child.exists():
-            continue
+    def _check_contents(self, directory=None):
+        """Check contents of directory (self.source_path or self.dest_path).
+        If checking source dir, it copies every file of dir to destination dir
+        if it isn't already there. If checking destination dir, it removes 
+        every file or dir which isn't in source dir."""
+        tree = self._get_dir_tree(directory)
+        for element in tree:
+            if directory == self.source_path:
+                element_path_to_check = Path(self.dest_path / element.relative_to(self.source_path))
+                if element_path_to_check.exists():
+                    if not element.is_dir():
+                        src_mtime = element.stat().st_mtime
+                        dest_mtime = element_path_to_check.stat().st_mtime
+                        if src_mtime != dest_mtime:
+                            shutil.copy2(element, element_path_to_check)
+                else:
+                    if element.is_dir():
+                        shutil.copytree(element, element_path_to_check)
+                    else:
+                        shutil.copy2(element, element_path_to_check)
+            
+            elif directory == self.dest_path:
+                element_path_to_check = Path(self.source_path / element.relative_to(self.dest_path))
+                if not element_path_to_check.exists():
+                    if element.is_dir():
+                        shutil.rmtree(element, ignore_errors=True)
+                    else:
+                        if element.is_file():
+                            element.unlink()
+    
+    def _get_dir_tree(self, path):
+        """Returns list of all files and subdirectories in given direcotry (even
+        all files and subfolders in subfolders etc.)"""
+        tree = [path]
+        for element in tree:
+            if element.is_dir():
+                for child in element.iterdir():
+                    tree.append(child)
+        return tree
+    
+    def synchronize(self):
+        """Synchronizes content of self.dest_path according to self.source_path."""
+        # Check if destination directory already exists (if this script already
+        # run so part of the source directory is copied.
+        if not self.dest_path.is_dir():
+            # Destination directory doesn't exist so copy the whole source directory
+            # tree
+            shutil.copytree(self.source_path, self.dest_path)
+            
         else:
-            if child.is_dir():
-                shutil.copytree(child, dest_path_of_child)
-            else:
-                shutil.copy2(child, dest_path)
+            # Destination dir exists so call _check_contents for self.dest_path
+            # and after that for self.source_path.
+            self._check_contents(directory=self.dest_path)
+            self._check_contents(directory=self.source_path)
+                    
+if __name__ == '__main__':
+    synch = SynchDirs(Path(sys.argv[1]), Path(sys.argv[2]))
+    synch.synchronize()
